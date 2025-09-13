97334328c96c0b32122fbda9b5358e73cb5126a0
Juanjo0828 <jjcastrillongiraldo@gmail.com>
2025-09-13 14:53:12 -0500

    (develop) inicio validación con expresiones regulares

---
+++ helpers/validarregistro.js
@@ -0,0 +1,19 @@
+/*let inputs = document.querySelectorAll("formulario_entrada")
+imputs.map((input)=> {
+    console.log(input)})
+*/
+
+//inputs.map(funtion(){})
+
+let imputs = document.getElementsByClassName
+("formulario_entrada")
+
+let arregloImputs = [...imputs]
+console.log(imputs)
+console.log(arregloImputs)
+
+arregloImputs.map((imput)=> {
+    console.log(imput)
+})
+
+// ... expres opereitor: convierte a un elemnto iterable o que se puede contar
