4992f79f8ba285545e2d276c843a45dbe8d1e586
johanaph37 <johanapenah@gmail.com>
2025-09-20 14:49:38 -0500

    (develop): validación de formulario con expresiones regulares

--- helpers/validarregistro.js
+++ helpers/validarregistro.js
@@ -1,3 +1,5 @@
+import {expresiones} from './expresiones.js'
+
 /*let inputs = document.querySelectorAll("formulario_entrada")
 imputs.map((input)=> {
     console.log(input)})
@@ -5,15 +7,45 @@ imputs.map((input)=> {
 
 //inputs.map(funtion(){})
 
-let imputs = document.getElementsByClassName
-("formulario_entrada")
+let inputs = document.getElementsByClassName("formulario__entrada")
+let arregloInputs = [...inputs]
+arregloInputs.map((input)=>{
+input.addEventListener("keyup", (e)=>{
+    console.log(e)
+    switch (e.target.id) {
+        case "nombre":
+            if(expresiones.texto.test(e.target.value)){
+                e.target.style.borderColor = "green"
+                //e.target.style.color = "green"
 
-let arregloImputs = [...imputs]
-console.log(imputs)
-console.log(arregloImputs)
+            }else{
+               document.getElementById("nombre").style.borderColor = "red"
+            }
+            break;
+        case "apellido":
+        if(expresiones.texto.test(e.target.value)){
+            e.target.classList.add("correcto")
+            e.target.classList.remove("incorrecto")
+        }else{
+            e.target.classList.add("incorrecto")
+            e.target.classList.remove("correcto")
+        }
+        break;
+    
+    case "documento":
+        if(expresiones.numero.test(e.target.value)){
+            e.target.classList.add("correcto")
+            e.target.classList.remove("incorrecto")
+        }else{
+            e.target.classList.add("incorrecto")
+            e.target.classList.remove("correcto")
+        }
+        break;
+    }
 
-arregloImputs.map((imput)=> {
-    console.log(imput)
+
+})
+
 })
 
 // ... expres opereitor: convierte a un elemnto iterable o que se puede contar
